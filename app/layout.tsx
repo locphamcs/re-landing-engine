138ace9b225c6ff1435474f84ea3d07b8c322c5f
locpham <phamvanloc15199@gmail.com>
2026-03-06 15:57:04 +0900

    up google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -61,6 +62,21 @@ export default function RootLayout({
     >
       <body className="antialiased bg-background text-foreground">
         {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XBNW3CTXPN"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XBNW3CTXPN');
+            `}
+        </Script>
       </body>
     </html>
   );
